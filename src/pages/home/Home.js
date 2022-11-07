2a294e8a194e1da44e0a2cc8b929b4e1db70da1a
Tanvir1909R <trcr719@gmail.com>
2022-11-08 00:05:57 +0600

    nav and home banner

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,15 @@ import './home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='banner'>
+      <div className="Container">
+          <div className="bannerText">
+            <h1>PhotoPia Photography Service</h1>
+            <p>I am here for your quality photography services</p>
+            <button>Hire</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
