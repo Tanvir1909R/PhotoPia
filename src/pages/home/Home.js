809ffc4284cf6fa6d5e15faed90c17bc339ef3e0
Tanvir1909R <trcr719@gmail.com>
2022-11-08 13:01:37 +0600

    add about section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,58 @@
-import React from 'react'
-import './home.scss'
+import React from "react";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <section className='banner'>
-      <div className="Container">
-          <div className="bannerText">
-            <h1>PhotoPia Photography Service</h1>
-            <p>I am here for your quality photography services</p>
-            <button>Hire</button>
+    <>
+      <section>
+        <div className="banner">
+          <div className="Container">
+            <div className="bannerText">
+              <h1>PhotoPia Photography Service</h1>
+              <p>I am here for your quality photography services</p>
+              <button>Hire</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
-  )
-}
+        <div className="aboutSec">
+          <div className="Container">
+            <div className="aboutWrapper">
+              <div className="aboutText">
+                <div className="title">
+                  <p>WELCOME</p>
+                  <h1>About PhotoPia</h1>
+                </div>
+                <p className="aboutPera">
+                  All kinds of photography I make for the client. the other
+                  services I provide include video editing, video production,
+                  timelapse, etc. More information down below
+                </p>
+                <div className="workInfo">
+                  <div>
+                    <h2>100+</h2>
+                    <p>Successful Project</p>
+                  </div>
+                  <div>
+                    <h2>500+</h2>
+                    <p>World Wide Client</p>
+                  </div>
+                  <div>
+                    <h2>8+</h2>
+                    <p>Years Experience</p>
+                  </div>
+                </div>
+                <button>Learn more</button>
+              </div>
+              <div className="aboutImg">
+                <img src="./img/about-1.jpg" alt="drone with man" />
+                <img src="./img/about-2.jpg" alt="drone" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
