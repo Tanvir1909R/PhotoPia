01af4b905af90a575eb4f215c194664658478260
Tanvir1909R <trcr719@gmail.com>
2022-11-09 19:37:36 +0600

    add delete review function

--- src/pages/myReview/MyReview.js
+++ src/pages/myReview/MyReview.js
@@ -1,25 +1,41 @@
 import React, { useContext, useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import { authContext } from "../../contexts/UserContext";
-import Loader from '../../components/spinner/Loader'
+import Loader from "../../components/spinner/Loader";
+import { AiFillDelete } from "react-icons/ai";
+import { GrDocumentUpdate } from "react-icons/gr";
 import "./myreview.scss";
 
 const MyReview = () => {
-    const { user } = useContext(authContext);
+  const { user } = useContext(authContext);
   const [reviews, setReviews] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    setLoading(true)
-    fetch(`http://localhost:7000/reviews?email=${user.email}`)
+    setLoading(true);
+    fetch(`https://service-review-server.vercel.app/reviews?email=${user.email}`)
       .then((res) => res.json())
       .then((data) => {
         setReviews(data);
-        setLoading(false)
+        setLoading(false);
       });
-  }, []);
+  }, [user.email]);
 
-  if(loading){
-    return <Loader/>
+  const handleDelete = (id) => {
+    fetch(`https://service-review-server.vercel.app/reviews/${id}`,{
+        method:'delete'
+    })
+    .then(res => res.json())
+    .then(data =>{
+        if(data.acknowledged){
+            const filterReviews = reviews.filter(review => review._id !== id);
+            setReviews(filterReviews)
+        }
+    })
+  };
+
+  if (loading) {
+    return <Loader />;
   }
 
   return (
@@ -38,17 +54,29 @@ const MyReview = () => {
               <th>Service Name</th>
               <th>Message</th>
               <th>Rating</th>
+              <th>#</th>
             </tr>
           </thead>
           <tbody>
             {reviews.map((review, idx) => {
-
               return (
-                <tr>
+                <tr key={review._id}>
                   <td>{idx + 1}</td>
                   <td>{review.serviceName}</td>
                   <td>{review.message}</td>
                   <td>{review.rating}</td>
+                  <td>
+                    <AiFillDelete
+                      role="button"
+                      className="fs-5 me-2"
+                      onClick={()=> handleDelete(review._id)}
+                    />
+                    <GrDocumentUpdate
+                      role="button"
+                      className="fs-5"
+                      title="Update"
+                    />
+                  </td>
                 </tr>
               );
             })}
